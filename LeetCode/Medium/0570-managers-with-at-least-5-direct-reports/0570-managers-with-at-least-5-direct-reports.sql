d9864d2926be00e5e525200d0d363030028373a6
Ngoc My Nguyen (Jamie) <nngmyb6@gmail.com>
2026-02-12 21:48:14 +0000

    Time: 329 ms (27.28%), Space: 0B (100%) - LeetHub

--- LeetCode/Medium/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
+++ LeetCode/Medium/0570-managers-with-at-least-5-direct-reports/0570-managers-with-at-least-5-direct-reports.sql
@@ -1,4 +1,9 @@
 -- Write your PostgreSQL query statement below
-SELECT name FROM Employee
-WHERE id IN
-(SELECT managerId FROM Employee GROUP BY managerId HAVING count(id)>=5);
+-- Get name of manager by self join
+select m.name
+from employee e
+join employee m
+on e.managerId = m.id
+-- Group by each manager id and count employee id >=5
+group by e.managerId, m.name
+having count(e.id)>=5
